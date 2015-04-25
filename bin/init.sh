b44b91a224fa922de90b9c0edc5571917a2f087e
Jorge Andrade <j.andrade.dev@gmail.com>
2015-04-25 01:13:07 -0500

    update project

--- bin/init.sh
+++ bin/init.sh
@@ -5,13 +5,5 @@ mkdir app/src
 mkdir app/src/images
 mkdir app/src/js
 mkdir app/src/css
-mkdir assets
-mkdir assets/css
-mkdir assets/js
-mkdir assets/fonts
-mkdir layouts
-mkdir content
-mkdir pages
-mkdir partials
 bower update
 sudo npm install
